9590f634ddeca5f69e89ca6f34bfa4c00307d1ae
bigwizzo <wnnwillard@gmail.com>
2021-01-08 19:33:43 +0200

    Show all confirmed friends

---
+++ app/helpers/friendships_helper.rb
@@ -0,0 +1,10 @@
+module FriendshipsHelper
+  # def list_friends
+    # out = ''
+    # @friends.each do |f|
+      # puts f
+      # out << "<li>#{f.user.name} #{f.confirmed ? '' : ', pending'}<li>"
+    # end
+    # out.html_safe
+  # end
+end
