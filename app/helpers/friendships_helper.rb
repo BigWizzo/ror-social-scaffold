be8888b11e23573ed5db2c24051b18d91ebc4eb1
bigwizzo <wnnwillard@gmail.com>
2021-01-11 23:26:23 +0200

    Add friendship relationships

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,16 +1,2 @@
-module FriendshipsHelper
-  module FriendshipsHelper
-    def friend_status(user)
-      return if user == current_user
-  
-      if current_user.pending_friends.include?(user) || user.pending_friends.include?(current_user)
-        render 'friendships/pendind_friend_request'
-      elsif current_user.friend?(user) || user.friend?(current_user)
-        render 'friendships/already_friends'
-      else
-        render 'friendships/add_friend_button', user: user
-      end
-    end
-  end
-  
+module FriendshipsHelper  
 end
