2a92f26de60457286d0be7e9db9c2450f9e44ca0
thneves <tsneves11@gmail.com>
2021-01-18 18:46:13 -0300

    fix rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,36 +12,18 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  # confirm friends
-  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
-  # has_many :friends, through: :confirmed_friendships
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
 
-  # pending_friends
-  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
   has_many :pending_friends, through: :pending_friendships, source: :friend
-  
-  #friend_requests
-  has_many :inverted_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "friend_id"
+
+  has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverted_friendships, source: :user
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
     friends_array.compact
   end
-
-  # def pending_friends
-    # friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  # end
-
-  # def friend_requests
-    # inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
-  # end
-
-  # def confirm_friend(user)
-    # friendship = inverse_friendships.find { |friendship_confirm| friendship_confirm.user == user }
-    # friendship.confirmed = true
-    # friendship.save
-  # end
 
   def friend?(user)
     friends.include?(user)
